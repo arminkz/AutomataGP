d47d8afca7680c6ceda7b1bef3213275cbcd47ac
Armin Kazemi <arminkz@live.com>
2017-02-28 11:28:15 +0330

    GUI Improvement using MahApps

--- AutomataGP/MainWindow.xaml.cs
+++ AutomataGP/MainWindow.xaml.cs
@@ -14,13 +14,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace AutomataGP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
 
         public string[,] FSM { get; set; }
